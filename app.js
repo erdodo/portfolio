be225a85b2a81268796b9377b92119a4e4bcb078
erdodo <erdoganyesil3@gmail.com>
2023-03-30 07:54:25 +0300

    profile.json import

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const app = express();
+const bodyParser = require('body-parser')
+const profile = require('./profile.json')
 
-
+app.use(bodyParser.json());
 app.get("/", (req, res) => {
-    res.send("Hello world testr");
+    res.json(profile);
 });
 console.log(process.env.MONGODB_PASS);
 mongoose.connect(`mongodb+srv://erdoganyesil3:${process.env.MONGODB_PASS}@testdb.eo1tnko.mongodb.net/?retryWrites=true&w=majority`, {
