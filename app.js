efc1bd0bfb0ebc23d4cf1757801af048651d86ba
erdodo <erdoganyesil3@gmail.com>
2023-04-02 22:52:08 +0300

    Firebase bağlantısı

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
+    res.set('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -26,7 +27,44 @@ var dir = path.join(__dirname, 'public');
 app.use(express.static(dir));
 app.use('/', router);
 
+/*let db = new JsonDB(new Config("profile.json", true, false, '/'));
+app.get("/", async (req, res) => {
+    if(req.query.lang === "tr") {
+        var data = await db.getData("/");
+        res.json(data.tr)
+    } else {
+        var data = await db.getData("/");
+        res.json(data.en)
+    }
+})
+app.get("/env", async (req, res) => {
+    res.json(process.env)
+})
+app.get("/change", async (req, res) => {
+    await db.push("/"+req.query.key, req.query.value).then(async () => {
+        var data = await db.getData("/");
+        res.json(data)
+    }).catch((err) => {
+        res.send(err)
+    })
+});
+app.get("/project/:name", async (req, res) => {
+    let name = req.params.name.replace('%20',' ')
+    let data=[]
+    if(req.query.lang === "tr") {
+        data = await db.getData("/tr/projects");
+    } else {
+        data = await db.getData("/en/projects");
+    }
+    let projects=[]
+    data.forEach((project) => {
+        if(project.category.find((category) => category.toLowerCase() === name.toLowerCase())){
+            projects.push(project)
+        }
+    })
+    res.json(projects)
 
+});*/
 
 
 app.listen(80, () => {
--- router.js
+++ router.js
@@ -2,9 +2,7 @@ const express = require("express");
 const router = express.Router();
 const firebase = require("./firebaseConf");
 
-router.get("/", async (req, res) => {
-    res.render('index',{data:await firebase.getData(req.query.collection, req.query.docId)})
-})
+
 router.post("/", async (req, res) => {
     res.json(await firebase.getData(req.query.collection, req.query.docId))
 })
@@ -17,6 +15,10 @@ router.post("/set", async (req, res) => {
 router.delete("/delete", async (req, res) => {
     await firebase.deleteData(req.query.collection, req.query.docId)
     res.json(await firebase.getData(req.query.collection, req.query.docId))
+})
+router.get("/project/:name",  (req, res) => {
+   res.send("hello")
+
 })
 
 module.exports = router;
