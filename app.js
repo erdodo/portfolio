738bf8f19f245f17611b8caf548a16d83e4c16a0
erdodo <erdoganyesil3@gmail.com>
2023-04-02 09:11:12 +0300

    Initial commit

--- app.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const app = express();
-const {Noco} = require("nocodb");
-const httpServer = app.listen(process.env.PORT || 8080);
-app.use(await Noco.init({}, httpServer, app));
